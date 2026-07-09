f7c478911a0c07373dfe60218271a0d5cd8d17ed
missilehitrussia-glitch <missilehitrussia@gmail.com>
2026-07-09 10:30:23 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CopperAndWax from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CopperAndWax />
+  </React.StrictMode>
+);
